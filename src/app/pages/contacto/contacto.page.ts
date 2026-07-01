fa1ed64a1fe37e38562c5e9768d5db7b2c9f5a8b
marino <marino.menendez@gmail.com>
2026-07-01 13:21:43 +0200

    geolocalización y distancia

--- src/app/pages/contacto/contacto.page.ts
+++ src/app/pages/contacto/contacto.page.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons, IonButton, IonItem, IonTextarea } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.page.html',
   styleUrls: ['./contacto.page.scss'],
   standalone: true,
-  imports: [IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonTextarea, IonItem, IonButton, IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class ContactoPage implements OnInit {
+
+  correo = '';
+  mensaje = '';
+  enviado = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  enviar() {
+    console.log("method enviar()");
   }
 
 }
--- src/app/pages/nosotros/nosotros.page.ts
+++ src/app/pages/nosotros/nosotros.page.ts
@@ -1,20 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonButton } from '@ionic/angular/standalone';
+import { GeolocationService } from 'src/app/services/geolocation-service';
 
 @Component({
   selector: 'app-nosotros',
   templateUrl: './nosotros.page.html',
   styleUrls: ['./nosotros.page.scss'],
   standalone: true,
-  imports: [IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonBackButton]
+  imports: [IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonBackButton, IonButton]
 })
 export class NosotrosPage implements OnInit {
 
-  constructor() { }
+  oficinaLat:number = 50;
+  oficinaLon:number = -5;
+  actualLat:number  = 0;
+  actualLon:number  = 0;
+  distancia: number | null = null;
+
+  constructor(private geo: GeolocationService) { }
 
   ngOnInit() {
+  }
+
+  async verUbicacion() {
+    
+    const coordenadas = await this.geo.getCurrentPosition();
+
+    this.actualLat = coordenadas.latitude;
+    this.actualLon = coordenadas.longitude;
+  }
+
+  calcularDistancia() {
+    this.distancia = this.geo.calcularDistancia(this.oficinaLat, this.oficinaLon, this.actualLat, this.actualLon);
   }
 
 }
