f565e54e18a5cea13570514eb9177bce8ef64290
marino <marino.menendez@gmail.com>
2026-07-01 14:00:49 +0200

    formulario contacto con preferencias

--- src/app/pages/contacto/contacto.page.ts
+++ src/app/pages/contacto/contacto.page.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons, IonButton, IonItem, IonTextarea } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons, IonButton, IonItem, IonTextarea, IonInput } from '@ionic/angular/standalone';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.page.html',
   styleUrls: ['./contacto.page.scss'],
   standalone: true,
-  imports: [IonTextarea, IonItem, IonButton, IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonInput, IonTextarea, IonItem, IonButton, IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class ContactoPage implements OnInit {
 
-  correo = '';
-  mensaje = '';
+  correo:string = "";
+  mensaje:string = "";
   enviado = false;
 
   constructor() { }
@@ -21,8 +22,20 @@ export class ContactoPage implements OnInit {
   ngOnInit() {
   }
 
-  enviar() {
+  async enviar() {
     console.log("method enviar()");
+
+    await Preferences.set({
+      key: 'ultimoMensaje',
+      value: JSON.stringify({
+        correo: this.correo,
+        mensaje: this.mensaje
+      })
+    });
+
+    console.log("Datos enviados: ");
+    console.log((await Preferences.get({key:'ultimoMensaje'})).value);
   }
+
 
 }
